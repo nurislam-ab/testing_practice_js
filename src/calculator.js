eb23a24cce0ef80c4e9a9f5e4774ed1d173be235
Nurislam Ab <you@example.com>
2020-09-21 18:42:40 +0500

    Add calculator module

---
+++ src/calculator.js
@@ -0,0 +1,18 @@
+const Calculator = (() => {
+  const add = (a, b) => {
+    return a + b;
+  };
+
+  const subtract = (a, b) => {
+    return a - b;
+  };
+
+  const divide = (a, b) => {
+    return a / b;
+  };
+
+  const multiply = (a, b) => {
+    return a * b;
+  };
+
+})();
